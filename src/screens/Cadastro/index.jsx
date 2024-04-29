160038be0ec6b3f8645efa2426a9e45e21a62435
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-04-29 16:26:32 -0300

    aaa

--- src/screens/Cadastro/index.jsx
+++ src/screens/Cadastro/index.jsx
@@ -79,7 +79,9 @@ export default function Cadastro({ route }) {
 
   return (
     <View style={styles.container}>
-      <Title title="Cadastro" />
+      <View style={styles.title}>
+        <Text>Cadastro</Text>
+      </View>
       <View style={styles.form}>
         <TextInput
           style={styles.input}
