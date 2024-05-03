97e39abe209d18c9e10c9de8a3e4a27a5710f024
Marinamorais <Marinamorais.564@gmail.com>
2024-05-03 15:53:21 -0300

    create: css

---
+++ src/screens/Categoria/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#000",
+
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
+})
+
+export default styles;
