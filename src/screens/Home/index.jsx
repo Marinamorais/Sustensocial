75df66301827c2d3e67828993e72c8174101f487
Marinamorais <marinamorais@aluno.senai.br>
2024-05-08 07:35:44 -0300

    termino

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -19,7 +19,9 @@ export default function Home() {
 
             <Text style={styles.texto01}>
               O SustenSocial é um aplicativo que tem como objetivo incentivar a
-              prática de ações sustentáveis. como a ONG, a e colaboradores.
+              prática de ações sustentáveis. como a ONG, a e colaboradores. O
+              app é uma plataforma de conexão entre ONGs, colaboradores e
+              pessoas que desejam contribuir com a sustentabilidade do planeta.
             </Text>
             <View style={styles.content01}>
               <Image
